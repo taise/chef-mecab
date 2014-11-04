791b95615d9b568b922526207895d5702012b6e3
Yusaku Omasa <yusaku.omasa@livesense.co.jp>
2014-11-04 15:15:48 +0900

    Initial commit chef-mecab cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+mecab CHANGELOG
+===============
+
+This file is used to list changes made in each version of the mecab cookbook.
+
+0.1.0
+-----
+- [yusaku omasa] - Initial release of mecab
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'mecab'
+maintainer       'Yusaku Omasa'
+maintainer_email 'yusaku.omasa@livesense.co.jp'
+license          'All rights reserved'
+description      'Installs/Configures mecab'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
